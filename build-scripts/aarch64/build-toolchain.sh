6ac0a759e685e1dbc3176cde234c5c59c0b6b45f
Saint-Theana <uiniui@126.com>
2018-05-19 18:30:02 +0800

    .

---
+++ build-scripts/aarch64/build-toolchain.sh
@@ -0,0 +1,9 @@
+mkdir /opt/NDK
+cd /opt/NDK
+apt install wget -y
+wget https://dl.google.com/android/repository/android-ndk-r17-linux-x86_64.zip
+apt install unzip -y
+unzip https://dl.google.com/android/repository/android-ndk-r17-linux-x86_64.zip
+    */build/tools/make_standalone_toolchain.py \
+    --arch arm64 --api 27 --stl=libc++ \
+    --install-dir /opt/toolchain --force
---
+++ build-scripts/aarch64/configuration.sh
@@ -0,0 +1,23 @@
+HOST="aarch64-linux-android"
+PREFIX="/opt/usr/local"
+LOCAL_DIR="/opt/usr/local"
+
+
+./configure \
+    --host=$HOST \
+    --prefix=$PREFIX \
+    --disable-nls \
+    --without-gnutls \
+    --with-openssl \
+    --without-libxml2 \
+    --with-libz --with-libz-prefix=${LOCAL_DIR} \
+    --with-libexpat --with-libexpat-prefix=${LOCAL_DIR} \
+    --with-sqlite3 --with-sqlite3-prefix=${LOCAL_DIR} \
+    --with-libcares --with-libcares-prefix=${LOCAL_DIR} \
+     --with-libssh2 --with-libssh2-prefix=${LOCAL_DIR} \
+    --with-ca-bundle='/system/etc/ca-certificates.crt' \
+    LDFLAGS="-L$LOCAL_DIR/lib" \
+    PKG_CONFIG_PATH="$LOCAL_DIR/lib/pkgconfig" \
+    ARIA2_STATIC=yes \
+    CC=/opt/toolchain/bin/clang \
+    CXX=/opt/toolchain/bin/clang++
